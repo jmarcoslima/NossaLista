f7f345cae98b3731cf2698a15ff8046e018cc89a
eduardasteyn <eduarda.jen@gmail.com>
2019-12-14 20:41:15 -0300

    Alteração do SplashScreen, imagens, animação, mudança no manifest e Style

---
+++ app/src/main/java/com/example/nossalista/SplashScreen.java
@@ -0,0 +1,82 @@
+package com.example.nossalista;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Classe para a splash screen
+ * Colocando todos os métodos que vou ultilizar na activity
+ *
+ * @author  Eduarda <eduarda.jen@gmail.com>
+ */
+public class SplashScreen extends AppCompatActivity {
+    Animation moveimage;
+    ImageView image;
+    TextView texto;
+
+    /**
+     * Está criando uma nova Activity para implementar a Splash Screen
+     * Mostrando a splash screen
+     * Chamando a animacao da imagem
+     * Ultilizando O Handler para fazer um encadeamento de Menssagem/Metodo
+     * Ultilizando o Runnable para iniciar outra tarefa, no caso Mostra a MainActivity
+     * @param savedInstanceState
+     * @return void
+     * @author  Eduarda <eduarda.jen@gmail.com>
+     *
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iniciaMain();
+            }
+        }, 5000);
+
+        animacao();
+
+    }
+    /**
+     * Inicia a main activity após o surgimento da splash sreen
+     *
+     * Utilizando a intent para iniciar a main activity.
+     *
+     * @return void
+     *
+     *
+     */
+    private void iniciaMain() {
+        Intent intent = new Intent(
+                SplashScreen.this, MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+
+    }
+    /**Iniciando animações
+     *
+     * @return void
+     */
+
+    private void animacao() {
+        image = findViewById(R.id.image);
+        Animation AparecerTitulo = AnimationUtils.loadAnimation(this, R.anim.moveimage);
+        image.startAnimation(AparecerTitulo);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.explos);
+        findViewById(R.id.tx).startAnimation(animation);
+    }
+
+
+}
